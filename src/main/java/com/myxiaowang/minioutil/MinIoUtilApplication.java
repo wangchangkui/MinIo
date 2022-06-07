c37523c2c8068c0fc98911bae4d066aa17faaf49
Myk <17360402335@163.com>
2022-06-07 10:09:09 +0800

    初始化

---
+++ src/main/java/com/myxiaowang/minioutil/MinIoUtilApplication.java
@@ -0,0 +1,13 @@
+package com.myxiaowang.minioutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinIoUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinIoUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myxiaowang/minioutil/MinIoUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myxiaowang.minioutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIoUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
